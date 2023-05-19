f29094b82a9de5d6b345e236562d45c7d94de919
sainyaVishwakarma <104253247+sainyaVishwakarma@users.noreply.github.com>
2023-05-19 10:56:49 +0530

    Add files via upload

---
+++ src/17-may/GradComponent.js
@@ -0,0 +1,12 @@
+
+export default function GradComponent()
+{
+    return <div>
+        <ul>
+            <li>degree = b.tech</li>
+            <li>year =2022</li>
+            <li>Final year score=76.6%</li>
+            <li>uni= RLAU</li>
+        </ul>
+    </div>
+}
